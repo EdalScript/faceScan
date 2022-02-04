1c2030725b4eb8781a19ed4ca3646e8b7e39f497
Edgar Torres <edalscript@gmail.com>
2022-02-04 16:27:44 +0300

    AI next step

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
@@ -13,80 +14,18 @@ const particlesLoaded = (container) => {
   console.log(container);
 };
 
-const particles = {
-  fpsLimit: 120,
-  interactivity: {
-    events: {
-      onClick: {
-        enable: true,
-        mode: "push",
-      },
-      onHover: {
-        enable: true,
-        mode: "repulse",
-      },
-      resize: true,
-    },
-    modes: {
-      bubble: {
-        distance: 400,
-        duration: 2,
-        opacity: 0.8,
-        size: 40,
-      },
-      push: {
-        quantity: 4,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-    },
-  },
-  particles: {
-    color: {
-      value: "#ffffff",
-    },
-    links: {
-      color: "#ffffff",
-      distance: 150,
-      enable: true,
-      opacity: 0.5,
-      width: 1,
-    },
-    collisions: {
-      enable: true,
-    },
-    move: {
-      direction: "none",
-      enable: true,
-      outMode: "bounce",
-      random: false,
-      speed: 3,
-      straight: false,
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 800,
-      },
-      value: 80,
-    },
-    opacity: {
-      value: 0.5,
-    },
-    shape: {
-      type: "circle",
-    },
-    size: {
-      random: true,
-      value: 5,
-    },
-  },
-  detectRetina: true,
+function App() {
+  const [input, setInput] = useState('');
+  const [submit, setButtonSubmit] = useState('');
+
+const onInputChange = (event) => {
+  console.log(event.target.value);
 }
 
-function App() {
+const onButtonSubmit = () => {
+console.log('click')
+}
+
   return (
     <div className="App">
       <Particles
@@ -94,12 +33,82 @@ function App() {
         id="tsparticles"
         init={particlesInit}
         loaded={particlesLoaded}
-        options={{ particles }}
+        options={{   fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
       />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
     </div>
   );
 }
--- src/Components/ImageLinkForm/ImageLinkForm.jsx
+++ src/Components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./ImageLinkForm.css"
 
-export default function ImageLinkForm() {
+export default function ImageLinkForm({onInputChange, onButtonSubmit}) {
     return (
         <div>
             <p className="f3">
                 {'FaceScan App will detect faces in your pictures. Give it a try!'}
             </p>
             <div className="pa4 br3 shadow-5 form center">
-                <input className="f4 pa2 w-70 center" type="text" />
-                <button className="2-30 grow f4 link ph3 pv2 dib white bg-hot-pink">Detect</button>
+                <input className="f4 pa2 w-70 center" type="text" onChange={onInputChange} />
+                <button className="2-30 grow f4 link ph3 pv2 dib white bg-hot-pink" onClick={onButtonSubmit}>Detect</button>
             </div>
         </div>
     );
